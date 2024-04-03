050c71419a55576c4961de059fad60f3949ceca5
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-04-03 10:00:04 -0600

    Shows #initialize & it has a name & age works

---
+++ lib/passenger.rb
@@ -0,0 +1,9 @@
+class Passenger
+  attr_reader :name, :age
+
+  def initialize(data)
+    @name = data["name"]
+    @age = data["age"]
+  end
+
+end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -2,5 +2,42 @@ require 'pry'
 require './lib/passenger'
 
 RSpec.describe Passenger do
+  #   pry(main)> require './lib/passenger'
+  # # => true
+
+  it 'can initialize' do
+    passenger = Passenger.new({})
+    expect(passenger).to be_an_instance_of(Passenger)
+
+  end
+
+  it 'has a name & an age' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+    
+    expect(charlie.name).to eq("Charlie")
+    expect(charlie.age).to eq(18)
+    expect(taylor.name).to eq("Taylor")
+    expect(taylor.age).to eq(12)
+  end
   
+  # it 'is an adult' do
+  #   charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+  #   taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+    
+  #   expect(charlie.adult?).to eq(false)
+  # end
+
+  # # => true
+
+  # pry(main)> taylor.adult?
+  # # => false
+
+  # pry(main)> charlie.driver?
+  # # => false
+
+  # pry(main)> charlie.drive
+
+  # pry(main)> charlie.driver?
+  # # => true
 end
